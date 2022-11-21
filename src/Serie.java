c4025108e6d22cbd2b35c44bf84adcc3728e8c4a
Clarks1223 <pablo.uchuari@epn.edu.ec>
2022-11-21 11:16:53 -0500

    atributos de la clase serie constructor y geters y setter

--- src/Serie.java
+++ src/Serie.java
@@ -1,5 +1,55 @@
 public class Serie {
     String titulo;
-    String numTemporadas;
-    boolean entre
+    int numTemporadas;
+    boolean entregado;
+    String genero;
+    String creador;
+
+    public Serie(){
+        titulo="";
+        creador="";
+        numTemporadas=3;
+        entregado=false;
+        genero="";
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public int getNumTemporadas() {
+        return numTemporadas;
+    }
+
+    public void setNumTemporadas(int numTemporadas) {
+        this.numTemporadas = numTemporadas;
+    }
+
+    public boolean isEntregado() {
+        return entregado;
+    }
+
+    public void setEntregado(boolean entregado) {
+        this.entregado = entregado;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public String getCreador() {
+        return creador;
+    }
+
+    public void setCreador(String creador) {
+        this.creador = creador;
+    }
 }
